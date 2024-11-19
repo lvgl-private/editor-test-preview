bde43edb90f0df03924b03485d5580097b2cda7a
Richard Gazdik <richard.gazdik+github@gmail.com>
2024-11-19 13:36:25 +0100

    fix: Fix project and asset paths

--- assets/javascript-C01jKx78.js
+++ assets/javascript-C01jKx78.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-DDaXDCAz.js";import"./index-DyzrPNSu.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-PGBG3Vkk.js";import"./index-Nh0v8pwC.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.51.0(67d664a32968e19e2eb08b696a92463804182ae4)
  * Released under the MIT license
--- assets/xml-DAPeRd35.js
+++ assets/xml-DAPeRd35.js
@@ -1,4 +1,4 @@
-import{m as r}from"./index-DyzrPNSu.js";/*!-----------------------------------------------------------------------------
+import{m as r}from"./index-Nh0v8pwC.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.51.0(67d664a32968e19e2eb08b696a92463804182ae4)
  * Released under the MIT license
