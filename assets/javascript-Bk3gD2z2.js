f2d0370e56007e61d23a4966846de06b0f278d78
Richard Gazdik <richard.gazdik+github@gmail.com>
2024-11-19 13:18:57 +0100

    fix: Update project root path

--- assets/javascript-Bk3gD2z2.js
+++ assets/javascript-Bk3gD2z2.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-dionhe-6.js";import"./index-ud7ukzM8.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-DDaXDCAz.js";import"./index-DyzrPNSu.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.51.0(67d664a32968e19e2eb08b696a92463804182ae4)
  * Released under the MIT license
