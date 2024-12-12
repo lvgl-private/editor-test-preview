ab37bd9799650c6abfcec7230e1881dad2bd8593
Richard Gazdik <richard.gazdik+github@gmail.com>
2024-12-12 16:08:19 +0100

    fix: Fix preview content update issue

--- assets/javascript-B4ezib6Z.js
+++ assets/javascript-B4ezib6Z.js
@@ -1,4 +1,4 @@
-import{conf as t,language as e}from"./typescript-B9yUR6UP.js";import"./index-JAyphFv2.js";/*!-----------------------------------------------------------------------------
+import{conf as t,language as e}from"./typescript-CEQ4OfBz.js";import"./index-BwAQDYsv.js";/*!-----------------------------------------------------------------------------
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Version: 0.51.0(67d664a32968e19e2eb08b696a92463804182ae4)
  * Released under the MIT license
